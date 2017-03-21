e0d36a56ad6ae2cd15023814914ba1d3fd55ae68
Nircek <marcinek0302@gmail.com>
2017-03-21 20:12:11 +0100

    Support for PSQueue
    add support for PSQueue

--- pop.php
+++ pop.php
@@ -7,5 +7,37 @@ odczytuje wydarzenie z kolejki wydarzeń
 
 */
 require "admin.php";
+require_once "mysql.php";
+$connection = @new mysqli($db_host,$db_user,$db_pass,$db_table);
+if($connection->connect_errno!=0){
+	echo "+";
+	echo $connection->connect_errno;
+	exit;
+}
+$connection->set_charset("utf8");
+$result=$connection->query("SELECT * FROM `psqueue` ORDER BY `id` LIMIT 1");
+if(!$result){
+	echo "+";
+	echo $connection->connect_errno;
+	exit;
+}
+if(($result->num_rows)>0){
+	$r=$result->fetch_assoc();
+	$r=$r['name'];
+	$result=$connection->query("DELETE FROM `psqueue` ORDER BY `id` LIMIT 1 ");
+	if(!$result){
+		echo "+";
+		echo $connection->connect_errno;
+		exit;
+	}
+}else{
+	echo -1;
+	$connection->close();
+	exit;
+}
+$connection->close();
+echo 0;
+echo $r;
+exit;
 
 ?>
--- push.php
+++ push.php
@@ -6,6 +6,26 @@ PCF
 dodaje wydarzenie do kolejki wydarzeń
 
 */
+if(!isset($_GET['event'])){
+	echo -1;
+	exit;
+}
+require_once "mysql.php";
+$connection = @new mysqli($db_host,$db_user,$db_pass,$db_table);
+if($connection->connect_errno!=0){
+	echo "+";
+	echo $connection->connect_errno;
+	exit;
+}
+$connection->set_charset("utf8");
+$result = $connection->query("INSERT INTO `psqueue`(`id`,`name`) VALUES ('','".$_GET['event']."')");
+if($result){
+	echo "+";
+	echo $connection->connect_errno;
+	exit;
+}
 
-
+$connection->close();
+echo 0;
+exit;
 ?>
