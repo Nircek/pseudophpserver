3dfa673ef95a2afd1dfdf7c3535b7f01480eb58c
Nircek <marcinek0302@gmail.com>
2017-03-22 15:53:47 +0100

    Add PCQueue support

--- refresh.php
+++ refresh.php
@@ -6,6 +6,27 @@ PCF
 sprawdza info od serwera
 
 */
-
-
+require_once "login.php";
+require_once "mysql.php";
+$connection = @new mysqli($db_host,$db_user,$db_pass,$db_table);
+if($connection->connect_errno!=0){
+	echo "+";
+	echo $connection->connect_errno."".$connection->connect_error;
+	exit;
+}
+$connection->set_charset("utf8");
+$result=$connection->query(sprintf("SELECT * FROM `pcqueue` WHERE `user`='%s'",
+mysqli_real_escape_string($connection,$_GET['user'])));
+if(!$result){
+	echo "+";
+	echo $connection->errno."".$connection->error;
+	exit;
+}
+if(($result->num_rows)<1){
+	echo -1;
+	$connection->close();
+	exit;
+}
+echo 0;
+echo $result->fetch_assoc()['text'];
 ?>
--- reply.php
+++ reply.php
@@ -6,7 +6,56 @@ PSF
 odpowiada do klienta
 
 */
+if(!isset($_GET['text'],$_GET['user'])){
+	echo -1;
+	exit;
+}
 require "admin.php";
+require_once "mysql.php";
+$connection = @new mysqli($db_host,$db_user,$db_pass,$db_table);
+if($connection->connect_errno!=0){
+	echo "+";
+	echo $connection->connect_errno;
+	exit;
+}
+$connection->set_charset("utf8");
+
+$result=$connection->query(sprintf("SELECT * FROM pcqueue WHERE `user`='%s'",
+mysqli_real_escape_string($connection,$_GET['user'])));
+if(!$result){
+	echo "+";
+	echo $connection->errno."".$connection->error;
+	exit;
+}
+if($result->num_rows>0){
+	$result = $connection->query(sprintf("UPDATE pcqueue SET `text`='%s' WHERE `user`='%s'",
+		mysqli_real_escape_string($connection,$_GET['text']),
+		mysqli_real_escape_string($connection,$_GET['user'])));
+	if(!$result){
+	echo "+";
+	echo $connection->errno."".$connection->error;
+	exit;
+	}
+}else{
+	$result = $connection->query(sprintf("INSERT INTO pcqueue (`user`,`text`) VALUES('%s','%s')",
+	mysqli_real_escape_string($connection,$_GET['user']),
+	mysqli_real_escape_string($connection,$_GET['text'])
+	));
+	if(!$result){
+	echo "+";
+	echo $connection->errno."".$connection->error;
+	exit;
+}
+}
+if(!$result){
+	echo "+";
+	echo $connection->errno;
+	exit;
+}
+
+$connection->close();
+echo 0;
+exit;
 
 
 ?>
